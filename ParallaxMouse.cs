df59af4dd34d51d035a7761d6ecb06e26bc3b504
Liam <93839666+L1am-cd@users.noreply.github.com>
2024-11-05 20:46:35 +0100

    Fixed error with Ranges, added two commencts

--- ParallaxMouse.cs
+++ ParallaxMouse.cs
@@ -1,4 +1,4 @@
-// Script originally made by Liam (GitHub: L1am-cd) 2 November 2024
+// Script originally made by Liam (GitHub: L1am-cd) 5 November 2024
 // This script is free to edit and modify. 
 // If you use it, credit would be appreciated, but is not needed.
 
@@ -13,24 +13,26 @@ public class ParallaxMouse : MonoBehaviour
     public bool MoveOnX = true;
         public bool MoveOnY = true;
             public float parallaxStrength = -0.5f;
+
                 [Range(0f, 1f)] [Tooltip("Lower is slower and more smooth")]
                 public float smoothSpeedMouse = 0.03f;
+
                     [Range(0f, 1f)] [Tooltip("Lower is slower and more smooth")]
                     public float smoothSpeedTouch = 0.02f;
 
     [Header("Maximum Offset Settings")]
+    [Range(0f, 50f)] // Edit the last number if you need a wider range
     public float maxOffsetX = 0.5f;
+        [Range(0f, 50f)] // Edit the last number if you need a wider range
         public float maxOffsetY = 0.5f;
 
-    [Range(0f, 50f)]
     private Vector3 targetPosition;
+        private Vector3 initialPosition;
 
-    [Range(0f, 50f)]
-    private Vector3 initialPosition;
-
-    [Space(10)]
-    [Header("Information")]
+    [Space(10)] [Header("Information")]
     public Vector2 mousePosition;
+        public float offsetX;
+            public float offsetY;
 
     void Start()
     {
@@ -45,8 +47,8 @@ public class ParallaxMouse : MonoBehaviour
 
         mousePosition = Input.mousePosition;
             Vector3 viewportPosition = new Vector3(mousePosition.x / Screen.width, mousePosition.y / Screen.height, 0);
-                float offsetX = MoveOnX ? Mathf.Clamp((viewportPosition.x - 0.5f) * parallaxStrength, -maxOffsetX, maxOffsetX) : 0;
-                    float offsetY = MoveOnY ? Mathf.Clamp((viewportPosition.y - 0.5f) * parallaxStrength, -maxOffsetY, maxOffsetY) : 0;
+                offsetX = MoveOnX ? Mathf.Clamp((viewportPosition.x - 0.5f) * parallaxStrength, -maxOffsetX, maxOffsetX) : 0;
+                    offsetY = MoveOnY ? Mathf.Clamp((viewportPosition.y - 0.5f) * parallaxStrength, -maxOffsetY, maxOffsetY) : 0;
                         targetPosition = initialPosition + new Vector3(offsetX, offsetY, 0);
                             if (Input.touchCount > 0) transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeedTouch); // Touch Input
                             else transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeedMouse); // Mouse Input
